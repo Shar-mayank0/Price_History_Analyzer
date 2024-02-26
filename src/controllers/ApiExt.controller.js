09d496dcd9e5ac967a34fac51463252f02ebc187
Mayank Sharma <b230289@skit.ac.in>
2024-02-26 23:00:27 +0530

    Add product registration functionality to API controller

--- src/controllers/ApiExt.controller.js
+++ src/controllers/ApiExt.controller.js
@@ -1,8 +1,27 @@
 import { asynchandler } from "../utils/asynchandler.js";
 import {ApiUrl} from "../models/ApiUrl.model.js";
+import { apiError } from "../utils/ApiError.js";
 
 const registerProduct = asynchandler(async (req, res) => {
         res.json({ message: "Product registered" });
+        const body = req.body;
+        console.log(body);
+        const newProduct = {
+                site: body.Product_site,
+                prod_id: body.Product_ID,
+                url: body.Product_URL,
+        };
+        const prodExists = await ApiUrl.findOne({
+                $or: [{prod_id: newProduct.prod_id}, {url: newProduct.url}]
+        })
+        if (prodExists) {
+                throw new apiError(409, 'Product ID or URL already exists');
+        }
+        if (!newProduct.prod_id || !newProduct.url || !newProduct.site) {
+                throw new apiError(404, 'Product ID, URL or site are missing');
+        }
+        
+        await ApiUrl.create(newProduct);
 });
 
 export { registerProduct }
--- src/routes/ApiExt.routes.js
+++ src/routes/ApiExt.routes.js
@@ -3,7 +3,7 @@ import { registerProduct } from "../controllers/ApiExt.controller.js";
 
 const router = Router();
 
-router.route("/amazonproducts/url/:prod_id").post(registerProduct);
+router.route("/amazonproducts/url").post(registerProduct);
 
 // This is the default export for this module
 export default router;
